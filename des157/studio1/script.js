8aeca2b5ba5ef3597a843a83a8a159074cfffc15
Annabel Koshy <ackoshy@ucdavis.edu>
2021-04-28 16:26:57 -0700

    final

--- des157/studio1/script.js
+++ des157/studio1/script.js
@@ -16,9 +16,9 @@ var madlib = document.querySelector('#madlib');
     var myText_four; 
 
     if(noun1 && noun2 && noun3 && adj1 && adj2 && number){
-        myText = ` • ${noun1}'s maganizine named Donald Trump the most <b><u>${adj1}</u><b> man alive`; 
+        myText = ` • <b><u>${noun1}'s</u><b> maganizine named Donald Trump the most <b><u>${adj1}</u><b> man alive`; 
         myText_two = `• Trump released his tax returns, paying more than $<b><u>${number}</u><b> in taxes!`; 
-        myText_three = `• Donald Trump was recently spotted attending the <b><u>${noun3}</u><b> protest in ${noun2}`; 
+        myText_three = `• Donald Trump was recently spotted attending the <b><u>${noun3}</u><b> protest in <b><u>${noun2}<u><b>`; 
         myText_four = `MAKE AMERICA <b><u>${adj2}</u><b> AGAIN`; 
     }
    
