7161b9b4c5a9ff1c205c448a2d96e93581501187
Annabel Koshy <ackoshy@ucdavis.edu>
2021-05-11 02:56:22 -0700

    first

---
+++ des157/studio2/script.js
@@ -0,0 +1,58 @@
+(function () {
+
+    'use strict';
+    
+        document.getElementById("img1").addEventListener('click',function(event){
+        event.preventDefault(); 
+        document.getElementById('overlay_one').className='showing'; 
+    
+    });
+
+        document.querySelector('.close').addEventListener('click',function(event){
+            event.preventDefault(); 
+            document.getElementById('overlay_one').className='hidden'; 
+ 
+    }); 
+
+    document.addEventListener('keydown',function(event){
+        if(event.key=='Escape'){
+            document.getElementById('overlay_one').className='hidden'; 
+        }
+    }); 
+    
+    document.getElementById("img2").addEventListener('click',function(event){
+        event.preventDefault(); 
+        document.getElementById('overlay_two').className='showing'; 
+    
+    });
+
+        document.querySelector('.close').addEventListener('click',function(event){
+            event.preventDefault(); 
+            document.getElementById('overlay_two').className='hidden'; 
+ 
+    }); 
+
+    document.addEventListener('keydown',function(event){
+        if(event.key=='Escape'){
+            document.getElementById('overlay_two').className='hidden'; 
+        }
+    }); 
+    document.getElementById("img3").addEventListener('click',function(event){
+        event.preventDefault(); 
+        document.getElementById('overlay_three').className='showing'; 
+    
+    });
+
+        document.querySelector('.close').addEventListener('click',function(event){
+            event.preventDefault(); 
+            document.getElementById('overlay_three').className='hidden'; 
+ 
+    }); 
+    document.addEventListener('keydown',function(event){
+        if(event.key=='Escape'){
+            document.getElementById('overlay_three').className='hidden'; 
+        }
+    }); 
+
+
+})();
